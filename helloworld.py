c0f4121fa71c375dda934a357bf8369d92495937
Shawn Zhang <xiaoen.zhang@mail.utoronto.ca>
2022-09-18 17:32:28 -0400

    first rebase commit

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,2 @@
 print("Hello World: I have been at UofT for 4 years.")
-<<<<<<< HEAD
-print("commit 1 for develop")
-=======
->>>>>>> b25af3a519bff82ce5a5d8be9ea00c9c8d95649b
+print("first commit for rebase branch")
